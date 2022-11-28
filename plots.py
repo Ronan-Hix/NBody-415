41d0deb0acf81c024667c3d0e78323ffd3d00201
Elaine Taylor <elainetaylor@wireless-10-104-192-168.umd.edu>
2022-11-28 15:47:58 -0500

    im trying my hardest here

--- plots.py
+++ plots.py
@@ -4,17 +4,22 @@ import os
 import pandas as pd
 from  matplotlib.colors import LogNorm
 import matplotlib
+import glob 
+import PIL
+from PIL import Image
+
+
 data_dir = './outputs'
 
 dataset = sorted(os.listdir(data_dir))[::20]
 
-for file in file dataset:
+for file in dataset:
     snapshot_data = pd.read_csv(os.path.join(data_dir, file))
     # snapshot_data = snapshot_data.to_numpy(snapshot_data)
     snapshot_data = np.loadtxt(os.path.join(data_dir, file), 
                                delimiter=",",
                                usecols=[1,2,3,4,5,6])
-
+    output_number = file.split("_")[1].split(".")[0]
     x = snapshot_data[:,0]
     y = snapshot_data[:,1]
     z = snapshot_data[:,2]
@@ -43,5 +48,14 @@ for file in file dataset:
                 )
     ax.set_facecolor(matplotlib.cm.Greys_r(0))
     ax.set(xlim=(-100,100), ylim=(-100,100))
-    plt.savefig('image_', dpi=300)
+    plt.savefig("./images/{}.png".format(output_number), dpi=300)
 
+fp_in = "./{}*.png".format("./images/")
+fp_out = "./{}_{}.gif".format(x, y)
+print(fp_in)
+img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
+print("Processing Frames to a gif...")
+img.save(
+     fp=fp_out, format="GIF", append_images=imgs, save_all=True, duration=4, loop=0
+ )
+print("Image saved to", fp_out)
