f335c539e1b6d2e0771b502fef02d3c6e93a138e
Fred <fgarcia4@umd.edu>
2022-12-01 02:24:56 -0500

    rerun

--- plots.py
+++ plots.py
@@ -6,125 +6,83 @@ from matplotlib.colors import LogNorm
 import matplotlib
 
 
-<<<<<<< Updated upstream
-data_dir = '/Users/elainetaylor/Desktop/ASTR415/Final Project /NBody-415/outputs'
+# data_dir = "./outputs"
 
-dataset = sorted(os.listdir(data_dir)) #[::20]
+# dataset = sorted(os.listdir(data_dir))  # [:20:10]  # [::20]
+# numbins = 200
+# with plt.style.context("dark_background"):
+#     for file in dataset:
+#         print(file)
+#         snapshot_data = pd.read_csv(os.path.join(data_dir, file))
+#         # snapshot_data = snapshot_data.to_numpy(snapshot_data)
+#         snapshot_data = np.loadtxt(
+#             os.path.join(data_dir, file), delimiter=",", usecols=[1, 2, 3, 4, 5, 6]
+#         )
+#         output_number = file.split("_")[1].split(".")[0]
+#         x = snapshot_data[:, 0]
+#         y = snapshot_data[:, 1]
+#         z = snapshot_data[:, 2]
+#         vx = snapshot_data[:, 3]
+#         vy = snapshot_data[:, 4]
+#         vz = snapshot_data[:, 5]
 
-for file in dataset:
-    snapshot_data = pd.read_csv(os.path.join(data_dir, file))
-    # snapshot_data = snapshot_data.to_numpy(snapshot_data)
-    snapshot_data = np.loadtxt(os.path.join(data_dir, file), 
-                               delimiter=",",
-                               usecols=[1,2,3,4,5,6])
-    output_number = file.split("_")[1].split(".")[0]
-    x = snapshot_data[:,0]
-    y = snapshot_data[:,1]
-    z = snapshot_data[:,2]
-    vx = snapshot_data[:,3]
-    vy = snapshot_data[:,4]
-    vz = snapshot_data[:,5]
-    
-    fig, ax = plt.subplots(1, 1, figsize=(4,4), dpi=300) 
-    #ax.scatter(x,y, s=0.2)
-    #ax.set(xlim=(-100,100), ylim=(-100,100))
-    
-    xy, _, _ = np.histogram2d(
-                    x,
-                    y,
-                    bins=500,
-                    normed=False,
-                    range=[[-100, 100], [-100, 100]],
-                    
-                )
-    ax.imshow(xy,
-                    cmap="inferno",
-                    interpolation="gaussian",
-                    origin="lower",
-                    extent=[-100, 100, -100, 100],
-                    norm= LogNorm()
-                )
-    ax.set_facecolor(matplotlib.cm.Greys_r(0))
-    ax.set(xlim=(-100,100), ylim=(-100,100))
-    plt.savefig("./images/{}.png".format(output_number), dpi=300)
-    
-=======
-data_dir = "./outputs"
+#         fig, ax = plt.subplots(1, 1, figsize=(5, 5), dpi=300)
+#         # ax.scatter(x,y, s=0.2)
+#         # ax.set(xlim=(-100,100), ylim=(-100,100))
 
-dataset = sorted(os.listdir(data_dir))  # [:20:10]  # [::20]
-numbins = 200
-with plt.style.context("dark_background"):
-    for file in dataset:
-        snapshot_data = pd.read_csv(os.path.join(data_dir, file))
-        # snapshot_data = snapshot_data.to_numpy(snapshot_data)
-        snapshot_data = np.loadtxt(
-            os.path.join(data_dir, file), delimiter=",", usecols=[1, 2, 3, 4, 5, 6]
-        )
-        output_number = file.split("_")[1].split(".")[0]
-        x = snapshot_data[:, 0]
-        y = snapshot_data[:, 1]
-        z = snapshot_data[:, 2]
-        vx = snapshot_data[:, 3]
-        vy = snapshot_data[:, 4]
-        vz = snapshot_data[:, 5]
+#         xy, _, _ = np.histogram2d(
+#             x,
+#             y,
+#             bins=numbins,
+#             normed=False,
+#             range=[[-100, 100], [-100, 100]],
+#         )
+#         surf_density = ax.imshow(
+#             xy,
+#             cmap="viridis",
+#             interpolation="gaussian",
+#             origin="lower",
+#             extent=[-100, 100, -100, 100],
+#             # vmin=0,
+#             # vmax=150,
+#             norm=LogNorm(),
+#         )
 
-        fig, ax = plt.subplots(1, 1, figsize=(5, 5), dpi=300)
-        # ax.scatter(x,y, s=0.2)
-        # ax.set(xlim=(-100,100), ylim=(-100,100))
+#         ax.set_facecolor(matplotlib.cm.viridis(0))
+#         ax.set(
+#             xlim=(-80, 80),
+#             ylim=(-80, 80),
+#             xticklabels=[],
+#             yticklabels=[],
+#         )
 
-        xy, _, _ = np.histogram2d(
-            x,
-            y,
-            bins=numbins,
-            normed=False,
-            range=[[-100, 100], [-100, 100]],
-        )
-        surf_density = ax.imshow(
-            xy,
-            cmap="viridis",
-            # interpolation="gaussian",
-            origin="lower",
-            extent=[-100, 100, -100, 100],
-            # vmin=0,
-            # vmax=150,
-            norm=LogNorm(),
-        )
+#         cbar_ax = ax.inset_axes([0.10, 0.10, 0.45, 0.040])
+#         dens_cbar = fig.colorbar(
+#             surf_density,
+#             cax=cbar_ax,
+#             pad=-1,
+#             orientation="horizontal",
+#         )
+#         cbar_ax.set_title(
+#             label=r"$\log \: \mathrm{\Sigma}$" "\n",
+#             fontsize=10,
+#             pad=-3,
+#         )
 
-        ax.set_facecolor(matplotlib.cm.viridis(0))
-        ax.set(
-            xlim=(-80, 80),
-            ylim=(-80, 80),
-            xticklabels=[],
-            yticklabels=[],
-        )
-
-        cbar_ax = ax.inset_axes([0.10, 0.10, 0.45, 0.040])
-        dens_cbar = fig.colorbar(
-            surf_density,
-            cax=cbar_ax,
-            pad=-1,
-            orientation="horizontal",
-        )
-        cbar_ax.set_title(
-            label=r"$\log \: \mathrm{\Sigma}$" "\n",
-            fontsize=10,
-            pad=-3,
-        )
-
-        ax.tick_params(axis="both", labeltop="on", direction="in", which="both")
-        plt.savefig(
-            "./images/{}.png".format(output_number),
-            dpi=300,
-            bbox_inches="tight",
-            pad_inches=0.0,
-        )
-        plt.close()
+#         ax.tick_params(axis="both", labeltop="on", direction="in", which="both")
+#         plt.savefig(
+#             "./images_el/{}.png".format(output_number),
+#             dpi=300,
+#             bbox_inches="tight",
+#             pad_inches=0.0,
+#         )
+#         plt.close()
 
 #%%
 import glob
 from PIL import Image
 
-frames_dir = "./images/"
+frames_dir = "./images_el/"
 output = "test"
 
 fp_in = "./{}*.png".format(frames_dir)
@@ -135,4 +93,4 @@ img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
 print("Processing Frames to a gif...")
 img.save(fp=fp_out, format="GIF", append_images=imgs, save_all=True, duration=4, loop=0)
 print("Image saved to", fp_out)
->>>>>>> Stashed changes
+
